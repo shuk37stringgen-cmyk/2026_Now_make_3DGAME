3c577561ba722db8cc90eb24a40f92fd4f88dca1
shuk37stringgen-cmyk <shuk37string.gen@gmail.com>
2026-05-28 19:57:16 +0900

    ライトの消去
    宇宙空間のゲームで影を大胆に用いると全体に光が当たらず暗くなるので、使わないから消しておく。今後つくる場合は新たにクラスとしてライトを作るかも

--- 000_GameDevelopment/program/GameManager.cpp
+++ 000_GameDevelopment/program/GameManager.cpp
@@ -2,16 +2,7 @@
 
 void GameManager::init() {
 
-    //// 1. まずメインライトを平行光源をつかうらしい
-    //ChangeLightTypeDir(VGet(0.0f, -1.0f, 1.0f));
-
-    //// 2. メインライトの高原を最大にする
-    //SetLightDifColor(GetColorF(1.0f, 1.0f, 1.0f, 1.0f));
-
-    //// 3. 全体を明るくして影をけす
-    //SetGlobalAmbientLight(GetColorF(1.0f, 1.0f, 1.0f, 1.0f));
-
-
+   
     SetUseLighting(FALSE);
 
 
